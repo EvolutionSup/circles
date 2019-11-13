1fa2c4a1539281c933d8b00e03fe8eecbcff970a
EvolutionSup <avanothermail@gmail.com>
2019-11-13 20:34:09 +0300

    final

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 import sys
+
 from random import randint
 from PyQt5 import uic
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication, QMainWindow
-from PyQt5.QtGui import QPainter, QPen
+from PyQt5.QtGui import QPainter, QPen, QColor
+from UI import Ui_MainWindow
 
 
-class Main(QMainWindow):
+class Main(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.draw = False
-        uic.loadUi('UI.ui', self)
+        self.setupUi(self)
         self.btn.clicked.connect(self.flag)
 
     def flag(self):
@@ -27,9 +29,11 @@ class Main(QMainWindow):
 
     def draw_circles(self, qp):
         r = randint(20, 100)  # радиус окружности
-        for i in range(randint(1, 20)):
+        for i in range(randint(1, 200)):
+            qp.setRenderHint(QPainter.Antialiasing)
             qp.setPen(QPen(Qt.yellow, 5, Qt.SolidLine))
-            qp.drawEllipse(randint(10, 400), randint(10, 400), r, r)
+            qp.setPen(QColor(randint(0, 255), randint(0, 255), randint(0, 255)))
+            qp.drawEllipse(randint(25, 445), randint(30, 520), r, r)
 
 
 if __name__ == '__main__':
